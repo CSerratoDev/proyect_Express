08da217d50769c63323e038c5f0ff9b3dd874c8a
CSerratoDev <cserrato.dev@gmail.com>
2025-03-07 15:40:43 -0600

    added parameters and scripts

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 
 app.get("/", (req, res, next) => {
     res.status(200);
-    res.send("Bienvenido");
+    res.send("Bienvenido bro");
+});
+
+app.get("/:name", (req,res,next) => {
+    var name = req.params.name;
+    res.status(200);
+    res.send("Hola, " + name);
 });
 
 app.listen(3000, () => {
